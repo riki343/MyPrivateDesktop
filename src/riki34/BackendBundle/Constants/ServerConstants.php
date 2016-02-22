da01a241faa9691522a12e84a0cf40795404094f
riki34 <vladislav.kosko@gmai.com>
2016-02-23 00:17:47 +0200

    Fix to uploadImage in DesktopController

--- src/riki34/BackendBundle/Constants/ServerConstants.php
+++ src/riki34/BackendBundle/Constants/ServerConstants.php
@@ -3,7 +3,7 @@
 namespace riki34\BackendBundle\Constants;
 
 class ServerConstants {
-    const WEB_DIR = __DIR__ . '/../../../../web';
+    const WEB_DIR = 'http://desktop.dev';
     const FILESYSTEM_BASE_DIR = __DIR__ . '/../../../../web/files/filesystem';
     const FILESYSTEM_WEB_DIR = '/files/filesystem';
     const DEFAULT_BACKGROUND_IMAGE = '/images/1.jpg';
